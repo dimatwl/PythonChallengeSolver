609d102441e349490263b67dd4c800e9db8ef747
DimaTWL <dimatwl@gmail.com>
2013-10-06 21:43:20 +0400

    Small restructure.

--- src/MainSolver.py
+++ src/MainSolver.py
@@ -1,9 +1,11 @@
 __author__ = 'DimaTWL'
 
+from Solvers.SolverFor0 import SolverFor0
 
 class MainSolver:
     def __init__(self):
         pass
 
     def solve_all(self):
-        pass
+        solver = SolverFor0("http://www.pythonchallenge.com/pc/def/0.html")
+        solver.solve()
---
+++ src/Solvers/SolverFor0.py
@@ -0,0 +1,12 @@
+__author__ = 'DimaTWL'
+
+from AbstractSolver import AbstractSolver
+
+
+class SolverFor0(AbstractSolver):
+
+    def __init__(self, base_url):
+        AbstractSolver.__init__(self, base_url)
+
+    def solve(self):
+        pass
