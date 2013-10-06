2bb3e588064a3e63786fff903e76ce9e09280e25
DimaTWL <dimatwl@gmail.com>
2013-10-06 22:03:51 +0400

    Detalization on MainSolver

--- src/MainSolver.py
+++ src/MainSolver.py
@@ -4,8 +4,20 @@ from Solvers.SolverFor0 import SolverFor0
 
 class MainSolver:
     def __init__(self):
-        pass
+        self.solvers = []
+        self.base_url = "http://www.pythonchallenge.com/pc/def/0.html"
+        self.next_url = self.base_url
+        self.__register_solvers()
+
+    def __register_solvers(self):
+        self.solvers.append(SolverFor0)
 
     def solve_all(self):
-        solver = SolverFor0("http://www.pythonchallenge.com/pc/def/0.html")
-        solver.solve()
+        for solver_class in self.solvers:
+            solver = solver_class(self.next_url)
+            solver.solve()
+            if solver.get_next_url() is not None:
+                self.next_url = solver.get_next_url()
+            else:
+                raise ValueError
+
