7e4b877c6e1fe726bfdb418015a9db671f28d9a0
Dmitriy Shestavin <dshestavin@ea.com>
2013-10-11 21:36:13 +0400

    Parsing improvements.

--- src/Solvers/Helpers/UrlFetcher.py
+++ src/Solvers/Helpers/UrlFetcher.py
@@ -11,32 +11,32 @@ class MyHTMLParser(HTMLParser):
         self.__html_structure = []
 
     def handle_starttag(self, tag, attrs):
-        self.__html_structure.append(("Start_tag", (tag, [])))
+        self.__html_structure.append(("start_tag", (tag.lower(), [])))
         for attr in attrs:
-            self.__html_structure[-1][1][1].append(("Attr", attr))
+            self.__html_structure[-1][1][1].append(("attr", (attr[0].lower(), attr[1].lower())))
 
     def handle_endtag(self, tag):
-        self.__html_structure.append(("End_tag", tag))
+        self.__html_structure.append(("end_tag", tag.lower()))
 
     def handle_data(self, data):
-        self.__html_structure.append(("Data", data))
+        self.__html_structure.append(("data", data.lower()))
 
     def handle_comment(self, data):
-        self.__html_structure.append(("Comment", data))
+        self.__html_structure.append(("comment", data.lower()))
 
     def handle_entityref(self, name):
         c = chr(name2codepoint[name])
-        self.__html_structure.append(("Named_ent", c))
+        self.__html_structure.append(("named_ent", c.lower()))
 
     def handle_charref(self, name):
         if name.startswith('x'):
             c = chr(int(name[1:], 16))
         else:
             c = chr(int(name))
-        self.__html_structure.append(("Num_ent", c))
+        self.__html_structure.append(("num_ent", c.lower()))
 
     def handle_decl(self, data):
-        self.__html_structure.append(("Decl", data))
+        self.__html_structure.append(("decl", data.lower()))
 
     def get_html_structure(self):
         return self.__html_structure
@@ -52,7 +52,7 @@ class UrlFetcher:
     def fetch_url(self):
         request = urllib.request.Request(self.__url_parser.get_url())
         response = urllib.request.urlopen(request)
-        page_code = response.read()
-        self.__parser.feed(str(page_code))
+        page_code = response.read().decode("utf-8")
+        self.__parser.feed(page_code)
         print(self.__parser.get_html_structure())
         return page_code
