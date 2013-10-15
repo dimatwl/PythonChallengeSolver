360349f6ae897f6668351703a5803f929ab7c954
Dmitriy Shestavin <dshestavin@ea.com>
2013-10-15 17:38:58 +0400

    String constants for HTMLParser.

--- src/Solvers/Helpers/UrlFetcher.py
+++ src/Solvers/Helpers/UrlFetcher.py
@@ -7,36 +7,45 @@ from html.entities import name2codepoint
 
 class MyHTMLParser(HTMLParser):
     def __init__(self, strict = False):
+        self.__start_tag = "start_tag"
+        self.__attribute = "attribute"
+        self.__end_tag = "end_tag"
+        self.__data = "data"
+        self.__comment = "comment"
+        self.__entity_reference = "entity_reference"
+        self.__character_reference = "character_reference"
+        self.__declaration = "declaration"
+
         super(MyHTMLParser, self).__init__(strict=False)
         self.__html_structure = []
 
     def handle_starttag(self, tag, attrs):
-        self.__html_structure.append(("start_tag", (tag.lower(), [])))
+        self.__html_structure.append((self.__start_tag, (tag.lower(), [])))
         for attr in attrs:
-            self.__html_structure[-1][1][1].append(("attr", (attr[0].lower(), attr[1].lower())))
+            self.__html_structure[-1][1][1].append((self.__attribute, (attr[0].lower(), attr[1].lower())))
 
     def handle_endtag(self, tag):
-        self.__html_structure.append(("end_tag", tag.lower()))
+        self.__html_structure.append((self.__end_tag, tag.lower()))
 
     def handle_data(self, data):
-        self.__html_structure.append(("data", data.lower()))
+        self.__html_structure.append((self.__data, data.lower()))
 
     def handle_comment(self, data):
-        self.__html_structure.append(("comment", data.lower()))
+        self.__html_structure.append((self.__comment, data.lower()))
 
     def handle_entityref(self, name):
         c = chr(name2codepoint[name])
-        self.__html_structure.append(("named_ent", c.lower()))
+        self.__html_structure.append((self.__entity_reference, c.lower()))
 
     def handle_charref(self, name):
         if name.startswith('x'):
             c = chr(int(name[1:], 16))
         else:
             c = chr(int(name))
-        self.__html_structure.append(("num_ent", c.lower()))
+        self.__html_structure.append((self.__character_reference, c.lower()))
 
     def handle_decl(self, data):
-        self.__html_structure.append(("decl", data.lower()))
+        self.__html_structure.append((self.__declaration, data.lower()))
 
     def get_html_structure(self):
         return self.__html_structure
@@ -54,5 +63,6 @@ class UrlFetcher:
         response = urllib.request.urlopen(request)
         page_code = response.read().decode("utf-8")
         self.__parser.feed(page_code)
-        print(self.__parser.get_html_structure())
+        for entry in self.__parser.get_html_structure():
+            print(entry)
         return page_code
